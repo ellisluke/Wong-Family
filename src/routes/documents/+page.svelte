9ac3f62cc7ad78da43d11feb7120fc4acc3db254
ellisluke <lke8@cornell.edu>
2025-08-03 17:47:09 -0400

    deployment fixes for images and tailwind

--- src/routes/documents/+page.svelte
+++ src/routes/documents/+page.svelte
@@ -7,8 +7,8 @@
 <NavBar></NavBar>
 <div class="body">
 
-    <RightBanner title="Wong Sing Chew by A.K." desc="Read A.K. Wong's writing of his father's life" link="/documents/WongSingChewBio" image="/src/lib/images/documents/BiographyOfWongSingChew1.jpg" offset="-270" scale="1.1"></RightBanner>
-    <LeftBanner title="1930 Census" desc="A.K.'s answer to the U.S. Census" link="/documents/1930Census" image="/src/lib/images/documents/1930census.jpg" offset="-250" scale="2"></LeftBanner>
+    <RightBanner title="Wong Sing Chew by A.K." desc="Read A.K. Wong's writing of his father's life" link="/documents/WongSingChewBio" image="/images/documents/BiographyOfWongSingChew1.jpg" offset="-270" scale="1.1"></RightBanner>
+    <LeftBanner title="1930 Census" desc="A.K.'s answer to the U.S. Census" link="/documents/1930Census" image="/images/documents/1930census.jpg" offset="-250" scale="2"></LeftBanner>
 </div>
 <style>
     .body {
--- src/routes/documents/1930Census/+page.svelte
+++ src/routes/documents/1930Census/+page.svelte
@@ -2,7 +2,7 @@
     import SingleImageDoc from "../SingleImageDoc.svelte";
 
 </script>
-<SingleImageDoc title="1930 Census of A.K. Wong" date="April 5, 1930" image="/src/lib/images/documents/1930census.jpg" desc="
+<SingleImageDoc title="1930 Census of A.K. Wong" date="April 5, 1930" image="/images/documents/1930census.jpg" desc="
     On row 95 near the bottom, you can see A.K. Wong recorded as the head of his household. The next row lists Annie Wong (perhaps a nickname for Sun Lin Auyong?) as his wife, followed by daughters Lavinia and Wai Lan (Alana). Interestingly, 
     row 99 lists Wong Sing Chew as A.K.'s mother. Given that Wong Sing Chew had died three years prior to this census, it is possible his wife Hee Yee (who was about 62 years old at this time, matching the age and sex records) 
     took on his name for this census. Row 100, the last row shown here, records A.K.'s sister Sun Lin (but it is written as Sung Lin), also known as Aunty Lennie. Wong Sing Chew was 62 at the time of this census. A.K.'s daughter Fung
