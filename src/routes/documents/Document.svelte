52ab3737272fa70551f643d5f243b7c06f3669c3
ellisluke <lke8@cornell.edu>
2025-08-03 21:18:08 -0400

    fix: move Document component to better folder

---
+++ src/routes/documents/Document.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import { Carousel, Controls } from "flowbite-svelte";
+    let { images, title, date, description} = $props();
+</script>
+
+<div class="page">
+    <div class="document-container">
+        <Carousel {images} style="width: 100%; height: 900px;">
+            <Controls />
+        </Carousel>
+    </div>
+    <div class="document-details">
+        <div class="title">{title}</div>
+        <div class="date">{date}</div>
+        <div class="desc">{description}</div>
+    </div>
+</div>
+
+<style>
+    img {
+        width: 100%;
+    }
+    .page {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap; /* Allows wrapping */  
+        justify-content: flex-start; /* Aligns items to the left */
+    }
+    .document-container {
+        width: 50%;
+        height: 1000px;
+        /* background-color: blue; */
+        padding-top: 3%;
+    }
+
+    .document-details {
+        padding-left: 2%;
+        width: 48%;
+        height: 500px;
+        /* background-color: red; */
+        padding-top: 3%;
+    }
+
+    .title {
+        width: 100%;
+        height: 50px;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 20px;
+        /* background-color: green; */
+        color: black;
+    }
+
+    .date {
+        width: 100%;
+        height: 30px;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 15px;
+        /* background-color: orange; */
+        color: gray;
+    }
+
+    .desc {
+        width: 100%;
+        height: 300px;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 16px;
+        /* background-color: purple; */
+        color: black;
+    }
+</style>
