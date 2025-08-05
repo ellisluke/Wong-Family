cbad6e966d3ca9366f699f33285e2c19056df5f2
ellisluke <lke8@cornell.edu>
2025-08-04 23:52:07 -0400

    Feat: add submission directions

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,4 +2,96 @@
 
 </script>
 
-<h1>About page</h1>
+<div class="alvin-message">
+    <img src="/images/Alvin.png" alt="Alvin Wong in his garden">
+</div>
+<div class="contribute">
+    <p class="header">Contribute to This Project</p>
+    <br>
+    <p>
+        <strong>Gallery Submissions:</strong> send an email to gallery@WEBSITE.COM <br>
+        Each Gallery email submission should contain only one <i>high quality</i> scan or original of a <i>significant</i> image of our family members. 
+        This is not a place to submit somewhat random images of "everyday" occurances. 
+        These can be posted to our Facebook page.
+        <br> Examples of Gallery submissions: portraits, large group photos, rare or antique images of family members.
+    </p>
+    <br>
+    <p class="instructions">Please include the following in the email body accompanying each image.</p>
+    <ul class="instructions">
+        <li>- Title for the image</li>
+        <li>- Date of the image</li>
+        <li>- A description of the significance of the image and its circumstances</li>
+        <li>- The names of all family members appearing in the image</li>
+    </ul>
+    <br>
+    <p>
+        <strong>Document Submissions:</strong> send an email to documents@WEBSITE.COM <br>
+        Each Document email submission should contain only one <i>high quality</i> scan or original of a <i>significant</i> historical document relevant to our family. 
+        For now, all non-gallery media (i.e. images that don't contain a family member) can be submitted here.
+        We will review each document for validity and significance before posting it here. 
+        <br> Examples of Document submissions: government documents, family tree scans, letters, videos, rare writings or drawings.
+    </p>
+    <br>
+    <p class="instructions">Please include the following in the email body accompanying each image.</p>
+    <ul class="instructions">
+        <li>- Title for the image</li>
+        <li>- Date of the image</li>
+        <li>- A description of the significance of the image and its circumstances</li>
+        <li>- The names of all family members appearing in the image</li>
+    </ul>
+    <br>
+    <p class="other">All other suggestions, comments, or questions can be sent to INSERT@WEBSITE.COM</p>
+</div>
+<div class="contribute-code">
+    Contribute to the GitHub here:
+    <a href="https://github.com/ellisluke/Wong-Family">
+        <img src="/github.png" alt="GitHub Logo">
+    </a>
+</div>
+
+<style>
+    .alvin-message {
+        margin-left: 1%;
+        margin-top: 5%;
+        margin-bottom: 5%;
+        width: 98%;
+        border: 2px solid black;
+    }
+
+    .contribute {
+        width: 90%;
+        margin-left: 5%;
+    }
+
+    .header {
+        text-align: center;
+        font-size: 24px;
+        font-weight: bold;
+    }
+
+    .instructions {
+        width: 80%;
+        margin-left: 10%;
+    }
+
+    .other {
+        font-size: 18px;
+        text-align: center;
+    }
+
+    .contribute-code {
+        margin-top: 5%;
+        text-align: center;
+        font-size: 18px;
+        margin-bottom: 10%;
+    }
+
+    .contribute-code a img {
+        width: 200px;
+        margin-top: 10px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+
+</style>
