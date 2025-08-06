2aa8ec78417e7c17024afb06ba387064ac9a796e
ellisluke <lke8@cornell.edu>
2025-08-05 20:10:38 -0400

    feat: add yin sit sha video page

---
+++ src/routes/documents/YinSitShaDoc/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import VideoDoc from "../VideoDoc.svelte";
+
+</script>
+
+<VideoDoc 
+    title="Reflections of the Source" 
+    date="November 26, 2006" 
+    link="https://www.youtube.com/embed/44b1ljh9RjI?si=Nfu6nnJoIp9zr3FZ"
+    desc="This short documentary was created by the Punalu`u Yin Sit Sha Society for their Special Centennial Presentation."
+    ></VideoDoc>
