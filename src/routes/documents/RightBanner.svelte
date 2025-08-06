b8722c2955cf26128ed276019b7de74f9fe4e2f8
ellisluke <lke8@cornell.edu>
2025-08-05 20:29:29 -0400

    feat: add video to docs

--- src/routes/documents/RightBanner.svelte
+++ src/routes/documents/RightBanner.svelte
@@ -15,7 +15,7 @@
     .banner {
         width: 100%;
         height: 300px;
-        background-image: linear-gradient(to left, rgb(255, 222, 222), rgb(255, 78, 78));
+        background-image: linear-gradient(to right, rgb(255, 222, 222), rgb(155, 0, 0));
         overflow: hidden;
         position: relative;
     }
@@ -33,7 +33,7 @@
     }
 
     img {
-        opacity: 30%;
+        opacity: 40%;
         position: relative;
         filter: blur(3px);   
     }
