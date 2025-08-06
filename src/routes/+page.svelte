d3297e5597f3773fb8cb6500cca2f17a0dc2a9cd
ellisluke <lke8@cornell.edu>
2025-08-05 20:09:56 -0400

    fix: remove video from home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="body">
-    <iframe width="600" height="340" src="https://www.youtube.com/embed/44b1ljh9RjI?si=Nfu6nnJoIp9zr3FZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    
 </div>
 
 <style>
