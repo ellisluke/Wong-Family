d0218f0680198243b11332b8f708837a68440960
ellisluke <lke8@cornell.edu>
2025-08-06 19:18:31 -0400

    feat: add custom image/name nodes

---
+++ src/routes/MemberNode.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { Position, useSvelteFlow, type NodeProps, Handle } from '@xyflow/svelte';
+ 
+  let { id, data }: NodeProps = $props();
+ 
+  let { updateNodeData } = useSvelteFlow();
+</script>
+
+{#if data.top}
+  <Handle type="target" position={Position.Top} />
+{/if}
+
+{#if data.left}
+  <Handle type="target" position={Position.Left} />
+{/if}
+
+{#if data.right}
+  <Handle type="source" position={Position.Right} />
+{/if}
+
+{#if data.bottom}
+  <Handle type="source" position={Position.Bottom} />
+{/if}
+ 
+<div class="member-node">
+    <img src={data.image} alt={data.text}>
+  <p>{data.text}</p>
+</div>
+
+<style>
+  .member-node {
+    border: 2px solid black;
+    background-color: white;
+  }
+
+  .member-node p {
+    text-align: center;
+  }
+
+  img {
+    max-width: 300px;
+    max-height: 200px;
+  }
+</style>
