bf3d1c6c439e63fbad3f7605ff9cf1bc2f7a1a6b
ellisluke <lke8@cornell.edu>
2025-08-06 21:32:34 -0400

    feat: add three gallery imgs

---
+++ src/routes/gallery/gallery.js
@@ -0,0 +1,5 @@
+export const gallery = [
+    {title: "Wong Sing Chew", src: "/images/gallery/Wong Sing Chew.jpg", desc: "The only portrait of Wong Sing Chew" },
+    {title: "Wong Hee Yee", src: "/images/gallery/Wong Hee Yee.jpg", desc: "A portrait of Hee Yee, Wong Sing Chew's wife" },
+    {title: "Coyne St Portrait", src: "/images/gallery/HeeYeeFamilyCoyneSt.jpg", desc: "A portrait of Hee Yee, Wong Sing Chew's wife" },
+]
