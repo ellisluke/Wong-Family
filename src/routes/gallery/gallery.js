8fb1d9be9924ee1d06e6d4f7315b8e8f01d9010b
ellisluke <lke8@cornell.edu>
2025-08-06 21:34:58 -0400

    Add first gen img to gallery

--- src/routes/gallery/gallery.js
+++ src/routes/gallery/gallery.js
@@ -2,4 +2,5 @@ export const gallery = [
     {title: "Wong Sing Chew", src: "/images/gallery/Wong Sing Chew.jpg", desc: "The only portrait of Wong Sing Chew" },
     {title: "Wong Hee Yee", src: "/images/gallery/Wong Hee Yee.jpg", desc: "A portrait of Hee Yee, Wong Sing Chew's wife" },
     {title: "Coyne St Portrait", src: "/images/gallery/HeeYeeFamilyCoyneSt.jpg", desc: "A portrait of Hee Yee, Wong Sing Chew's wife" },
+    {title: "First Generation Wongs", src: "/images/gallery/FirstGenWongs.jpg", desc: "A gathering at a park of the first generation wongs. Taken October 21, 1978." },
 ]
