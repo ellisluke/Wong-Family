185032e3cb8ec36e9dc1eb894a7c2f8dff41a17c
ellisluke <lke8@cornell.edu>
2025-08-06 21:47:22 -0400

    feat: add more gallery images

--- src/routes/gallery/gallery.js
+++ src/routes/gallery/gallery.js
@@ -1,6 +1,14 @@
 export const gallery = [
-    {title: "Wong Sing Chew", src: "/images/gallery/Wong Sing Chew.jpg", desc: "The only portrait of Wong Sing Chew" },
-    {title: "Wong Hee Yee", src: "/images/gallery/Wong Hee Yee.jpg", desc: "A portrait of Hee Yee, Wong Sing Chew's wife" },
+    {title: "Wong Sing Chew", src: "/images/gallery/WongSingChew.jpg", desc: "The only portrait of Wong Sing Chew" },
+    {title: "Wong Hee Yee", src: "/images/gallery/WongHeeYee.jpg", desc: "A portrait of Hee Yee, Wong Sing Chew's wife" },
     {title: "Coyne St Portrait", src: "/images/gallery/HeeYeeFamilyCoyneSt.jpg", desc: "A portrait of Hee Yee, Wong Sing Chew's wife" },
     {title: "First Generation Wongs", src: "/images/gallery/FirstGenWongs.jpg", desc: "A gathering at a park of the first generation wongs. Taken October 21, 1978." },
+    {title: "Sun Hoo and Sun Moi", src: "/images/gallery/SunHooAndSunMoi.jpg", desc: "Sun Hoo and Sun Moi, the two oldest sisters." },
+    {title: "Kahn and Mew Lee 50th Anniversary", src: "/images/gallery/KahnMewLee50th1986.jpg", desc: "Kahn and Mew Lee took this photo to celebrate their 50th anniversary, 1986" },
+    {title: "Keye Luke in Hell's Half Acre", src: "/images/gallery/KeyeLukeHellsHalfAcre.png", 
+        desc: `This is an interesting photo, from Lane Hauck who found it somewhere.  Lane is married to Marilyn (Luke) who is the daughter of Lennie Luke and Edwin Luke.
+                Edwin is the brother of Keye Luke, the actor you may recognize from the Kung Fu series and many other productions.  Edwin was an actor as well as Social Worker.
+                In this (1954?) photo, the actors Leonard Strong and Keye Luke were filming on the set of the movie "Hell's Half Acre", filmed in Honolulu Chinatown when they 
+                took this picture of Keye and his relatives.  The second image names the persons in the photo (reversed, it was on the back of the photo).   From Left to Right:  
+                Alvin, Keye Luke, A.K. Wong, Alana, Sun Lin Wong, Lavinia and Leonard Strong.  Alvin was only 12 or 13 in this photo!` },
 ]
