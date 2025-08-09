f6151dba983c237604903daf134ae643fe46b249
ellisluke <lke8@cornell.edu>
2025-08-09 15:31:10 -0400

    fix: adjust family members

--- src/routes/nodes-and-edges.ts
+++ src/routes/nodes-and-edges.ts
@@ -1,7 +1,7 @@
 import type { Node, Edge } from '@xyflow/svelte';
 
-const genY = [0, 800, 1400]
-const scale = [1.0, 0.8, 0.6]
+const genY = [-100, 800, 1500]
+const scale = [1.1, 0.8, 0.6]
 const marriageX = 280;
 
 const offset = {
@@ -15,29 +15,29 @@ const offset = {
 
 export const initialNodes: Node[] = [
   // First Two Wongs
-  { id: '1', position: { x: 0, y: 0 }, type: 'memberNode', data: { text: 'Wong Sing Chew', image: '/images/home-tree/Wong Sing Chew.jpg', top: false, right: false, bottom: true, left: false, scale: scale[0]}},
-  { id: '2', position: { x: 400, y: 0 }, type: 'memberNode', data: { text: 'Wong Hee Yee', image: '/images/home-tree/Wong Hee Yee.jpg', top: false, right: false, bottom: true, left: false, scale: scale[0]}},
+  { id: '1', position: { x: 0, y: genY[0] }, type: 'memberNode', data: { text: 'Wong Sing Chew', image: '/images/home-tree/Wong Sing Chew.jpg', top: false, right: false, bottom: true, left: false, scale: scale[0]}},
+  { id: '2', position: { x: 400, y: genY[0] }, type: 'memberNode', data: { text: 'Wong Hee Yee', image: '/images/home-tree/Wong Hee Yee.jpg', top: false, right: false, bottom: true, left: false, scale: scale[0]}},
   // { id: '100', position: { x: 1000, y: 400 }, data: { label: 'base style 4' } }
 
   // First Gen Wongs
   { id: '3', position: { x: 200, y: 450 }, type: 'memberNode', data: { text: '1st Gen Wongs', image: '/images/home-tree/1stGenWongs.jpg', top: true, right: false, bottom: true, left: false, scale: scale[0]}},
   { id: '4', position: { x: offset.SunHoo, y: genY[1] }, type: 'memberNode', data: { text: 'Sun Hoo Wong', image: '/images/home-tree/TBD.jpg', top: true, right: true, bottom: true, left: false, scale: scale[1]}},
   { id: '5', position: { x: offset.SunMoi, y: genY[1] }, type: 'memberNode', data: { text: 'Sun Moi Wong', image: '/images/home-tree/TBD.jpg', top: true, right: true, bottom: true, left: false, scale: scale[1]}},
-  { id: '6', position: { x: offset.AhKwo, y: genY[1] }, type: 'memberNode', data: { text: 'Ah Kwo Wong', image: '/images/home-tree/TBD.jpg', top: true, right: false, bottom: true, left: false, scale: scale[1]}},
+  { id: '6', position: { x: offset.AhKwo, y: genY[1] }, type: 'memberNode', data: { text: 'Ah Kwo Wong', image: '/images/home-tree/AhKwoWong.jpg', top: true, right: false, bottom: true, left: false, scale: scale[1]}},
   { id: '7', position: { x: offset.Kahn, y: genY[1] }, type: 'memberNode', data: { text: 'Kahn Wong', image: '/images/home-tree/KahnWong.jpg', top: true, right: true, bottom: true, left: false, scale: scale[1]}},
   { id: '8', position: { x: offset.SunLin, y: genY[1] }, type: 'memberNode', data: { text: 'Sun Lin Wong', image: '/images/home-tree/TBD.jpg', top: true, right: true, bottom: true, left: false, scale: scale[1]}},
-  { id: '9', position: { x: offset.SunHop, y: genY[1] }, type: 'memberNode', data: { text: 'Sun Hop Wong', image: '/images/home-tree/TBD.jpg', top: true, right: true, bottom: true, left: false, scale: scale[1]}},
+  { id: '9', position: { x: offset.SunHop, y: genY[1] }, type: 'memberNode', data: { text: 'Sun Hop Wong', image: '/images/home-tree/SunHopWong.jpg', top: true, right: true, bottom: true, left: false, scale: scale[1]}},
 
   // First Gen Wongs Spouses
   { id: '10', position: { x: offset.SunHoo + marriageX, y: genY[1] }, type: 'memberNode', data: { text: 'Dai Sun Ching', image: '/images/home-tree/TBD.jpg', top: false, right: false, bottom: true, left: false, scale: scale[1]}},
   { id: '11', position: { x: offset.SunMoi + marriageX, y: genY[1] }, type: 'memberNode', data: { text: 'Tong Sing Ching', image: '/images/home-tree/TBD.jpg', top: false, right: false, bottom: true, left: false, scale: scale[1]}},
-  { id: '12', position: { x: offset.AhKwo + marriageX, y: genY[1] }, type: 'memberNode', data: { text: 'Sun Lin Auyong', image: '/images/home-tree/TBD.jpg', top: false, right: false, bottom: true, left: false, scale: scale[1]}},
+  { id: '12', position: { x: offset.AhKwo + marriageX, y: genY[1] }, type: 'memberNode', data: { text: 'Sun Lin Auyong', image: '/images/home-tree/SunLinAuyong.jpg', top: false, right: false, bottom: true, left: false, scale: scale[1]}},
   { id: '13', position: { x: offset.Kahn + marriageX, y: genY[1] }, type: 'memberNode', data: { text: 'Mew Lee Tong', image: '/images/home-tree/MewLeeTong.jpg', top: false, right: false, bottom: true, left: false, scale: scale[1]}},
   { id: '14', position: { x: offset.SunLin + marriageX, y: genY[1] }, type: 'memberNode', data: { text: 'Edwin Luke', image: '/images/home-tree/TBD.jpg', top: false, right: false, bottom: true, left: false, scale: scale[1]}},
   { id: '15', position: { x: offset.SunHop + marriageX, y: genY[1] }, type: 'memberNode', data: { text: 'Richard Luke', image: '/images/home-tree/TBD.jpg', top: false, right: false, bottom: true, left: false, scale: scale[1]}},
 
   // AK Wong Second Gen Wongs
-  { id: '16', position: { x: offset.AhKwo + 130, y: 1150 }, type: 'memberNode', data: { text: 'A.K. Wong Children', image: '/images/home-tree/AKWongs.jpg', top: true, right: false, bottom: true, left: false, scale: scale[1]}},
+  { id: '16', position: { x: offset.AhKwo + 130, y: 1250 }, type: 'memberNode', data: { text: 'A.K. Wong Children', image: '/images/home-tree/AKWongs.jpg', top: true, right: false, bottom: true, left: false, scale: scale[1]}},
   { id: '17', position: { x: offset.AhKwo - 500, y: genY[2] }, type: 'memberNode', data: { text: 'Lavinia Wong', image: '/images/home-tree/LaviniaWong.jpg', top: true, right: false, bottom: false, left: false, scale: scale[2]}},
   { id: '18', position: { x: offset.AhKwo - 200, y: genY[2] }, type: 'memberNode', data: { text: 'Alana Wong', image: '/images/home-tree/TBD.jpg', top: true, right: false, bottom: true, left: false, scale: scale[2]}},
   { id: '19', position: { x: offset.AhKwo + 100, y: genY[2] }, type: 'memberNode', data: { text: 'Fung Chun Wong', image: '/images/home-tree/TBD.jpg', top: true, right: false, bottom: false, left: false, scale: scale[2]}},
